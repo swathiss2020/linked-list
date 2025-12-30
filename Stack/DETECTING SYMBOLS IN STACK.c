916a3b79d0bbb88dff650639d5179656bdb3176f
swathiss2020 <swathi.ss2020@gmail.com>
2025-12-30 12:09:35 +0530

    Add symbol detection program in C
    Implement a program to detect if two strings have the same characters.

---
+++ Stack/DETECTING SYMBOLS IN STACK.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main() {
+    char a[100], b[100];
+    scanf("%s %s", a, b);
+    int f[256] = {0};
+    for(int i=0; a[i]; i++) f[(unsigned char)a[i]]++;
+    for(int i=0; b[i]; i++) f[(unsigned char)b[i]]--;
+    for(int i=0;i<256;i++){
+        if(f[i]!=0){
+            printf("NO");
+            return 0;
+        }
+    }
+    printf("YES");
+}
